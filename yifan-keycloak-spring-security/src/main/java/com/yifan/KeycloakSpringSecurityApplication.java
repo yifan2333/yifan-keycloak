81cd8070b08e5b3cfd9804ddd60a79b250a5afd0
wuyifan <wuyifan521>
2020-03-24 15:26:53 +0800

    添加yifan-keycloak-spring-security

---
+++ yifan-keycloak-spring-security/src/main/java/com/yifan/KeycloakSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.yifan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakSpringSecurityApplication.class, args);
+    }
+
+}
